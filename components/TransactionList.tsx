d249b7e002039458f3154138686158f9fc6c1994
Will <109585548+will-ixs@users.noreply.github.com>
2025-11-24 18:47:11 -0600

    Removed unsafe HTML rendering

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -68,7 +68,7 @@ export function TransactionList({ accountId }: TransactionListProps) {
                 </span>
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {transaction.description ? <span dangerouslySetInnerHTML={{ __html: transaction.description }} /> : "-"}
+                {transaction.description || "-"}
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
                 <span className={transaction.type === "deposit" ? "text-green-600" : "text-red-600"}>
